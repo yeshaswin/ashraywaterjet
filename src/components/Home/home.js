4126fe8b8c30afb2b99ef8f8ac0526391bbb27c9
yeshaswin <yeshaswin.krishna@gmaill.com>
2024-11-07 22:27:12 +0530

    update footer

---
+++ src/components/Home/home.js
@@ -0,0 +1,12 @@
+import NavBar from '../NavBar/navBar';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <NavBar/>
+    </div>
+  );
+}
+
+export default Home;
